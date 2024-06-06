bda54500d87c442a962849c0398e35a8dfa94065
Alian <iskra23231@gmail.com>
2024-06-06 23:34:40 +0300

    chore(frontend): update opacity and font size;

--- frontend/src/app/(main)/_components/PopularCities/CitiesItem.tsx
+++ frontend/src/app/(main)/_components/PopularCities/CitiesItem.tsx
@@ -6,24 +6,23 @@ interface ICityProps {
   city: ICityItem;
 }
 
-// EXPERIMENTAL SEO
 export const CityItem: FC<ICityProps> = ({ city: city }) => {
   return (
     <li className="relative w-full h-[140px] rounded-lg overflow-hidden bg-background-400">
-      <div className="absolute left-0 top-0 w-full h-full bg-background-300">
+      <div className="absolute left-0 top-0 w-full h-full">
         <img
           src={city.photo}
           alt={city.meta_desc}
           className="object-cover object-center w-full"
         />
-        <div className="absolute left-0 top-0 w-full h-full bg-primary opacity-40"></div>
+        <div className="absolute left-0 top-0 w-full h-full bg-primary opacity-20"></div>
       </div>
       <div className="absolute left-0 bottom-0 right-0 p-3 flex flex-col gap-[4px]">
         <Typography
           variant="span"
           transform="uppercase"
           width="semibold"
-          className="text-sm text-primary-100"
+          className="text-base text-primary-100"
         >
           {city.name}
         </Typography>
