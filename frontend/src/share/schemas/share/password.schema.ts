c7396de1842a3e140b8d488bf9a6ad6bf76a22d0
Alian <iskra23231@gmail.com>
2024-09-26 23:34:24 +0300

    fixup! refactor(frontend): auth system;

--- frontend/src/share/schemas/share/password.schema.ts
+++ frontend/src/share/schemas/share/password.schema.ts
@@ -1,14 +1,23 @@
-import { z } from "zod"
+import { z } from "zod";
 
-export const specialSymbols = "!@#$%^&*"
+export const specialSymbols = "!@#$%^&*`~";
 
 export const passwordScheme = z
 	.string()
-	.regex(/(?=.*[a-zA-Z])/g, "Должна быть хотя бы 1 латинская буква!")
-	.regex(/(?=.*[0-9])/g, "Должна быть хотя бы 1 цифра!")
-	.regex(new RegExp(`(?=.*[${specialSymbols}])`, "g"), `Пароль должен содержать хотя бы 1 символ: ${specialSymbols}`)
+	.regex(/(?=.*[a-zA-Z])/g, {
+		message: "Должна быть хотя бы 1 латинская буква!",
+	})
+	.regex(/(?=.*[0-9])/g, { message: "Должна быть хотя бы 1 цифра!" })
+	.regex(new RegExp(`(?=.*[${specialSymbols}])`, "g"), {
+		message: `Пароль должен содержать хотя бы 1 символ: ${specialSymbols}`,
+	})
 	.regex(
-		/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]*$/g,
-		"В пароле содержаться недопустимые символы",
+		new RegExp(
+			`^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[${specialSymbols}])[a-zA-Z0-9${specialSymbols}]`,
+			"g",
+		),
+		{
+			message: "В пароле содержаться недопустимые символы",
+		},
 	)
-	.min(6, "пароль не должен быть меньше 6 символов")
+	.min(6, { message: "пароль не должен быть меньше 6 символов" });
