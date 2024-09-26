8addcad470bf6044e158fc52c9931acd1dc939d3
Alian <iskra23231@gmail.com>
2024-09-26 23:30:36 +0300

    fix: auth;

--- frontend/src/app-configs/provider/modules/Auth/ClientProvider.tsx
+++ frontend/src/app-configs/provider/modules/Auth/ClientProvider.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 
-import { queryKeyFactory } from "@share/packages/serverActions"
-import type { User } from "@share/schemas"
-import { useQueryClient } from "@tanstack/react-query"
-import { useEffect, type FC, type PropsWithChildren } from "react"
+import { queryKeyFactory } from "@share/packages/serverActions";
+import type { User } from "@share/schemas";
+import { useQueryClient } from "@tanstack/react-query";
+import { type FC, type PropsWithChildren, useEffect } from "react";
 
 interface UserProviderProps extends PropsWithChildren {
-	user: User | null
+	user: User | null;
 }
 
 export const ClientProvider: FC<UserProviderProps> = ({ user, children }) => {
-	const queryClient = useQueryClient()
+	const queryClient = useQueryClient();
 
 	useEffect(() => {
 		if (!user) {
-			return
+			queryClient.setQueryData(queryKeyFactory.user(), null);
+		} else {
+			queryClient.setQueryData(queryKeyFactory.user(), user);
 		}
+	}, [queryClient, user]);
 
-		queryClient.setQueryData(queryKeyFactory.user(), user)
-	}, [queryClient, user])
-
-	return children
-}
+	return children;
+};
