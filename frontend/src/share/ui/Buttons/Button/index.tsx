268af8e1266961143ec80fe8763c097e1dcd0ce5
Alian <iskra23231@gmail.com>
2024-09-26 23:35:29 +0300

    fix: button with disabled status;

--- frontend/src/share/ui/Buttons/Button/index.tsx
+++ frontend/src/share/ui/Buttons/Button/index.tsx
@@ -1,27 +1,30 @@
-import { Slot } from "@radix-ui/react-slot"
-import * as React from "react"
+import { Slot } from "@radix-ui/react-slot";
+import * as React from "react";
 
-import { buttonVariants, type ButtonVariants } from "./variants"
+import { type ButtonVariants, buttonVariants } from "./variants";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, ButtonVariants {
-	asChild?: boolean
+interface ButtonProps
+	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+		ButtonVariants {
+	asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, variant, size, asChild = false, ...props }, ref) => {
-		const Comp = asChild ? Slot : "button"
+	({ className, variant, size, asChild = false, disabled, ...props }, ref) => {
+		const Comp = asChild ? Slot : "button";
 
 		return (
 			<Comp
-				type='button'
+				type="button"
 				ref={ref}
+				disabled={disabled}
 				className={buttonVariants({ variant, size, className })}
 				{...props}
 			/>
-		)
+		);
 	},
-)
+);
 
-Button.displayName = "Button"
+Button.displayName = "Button";
 
-export { Button, buttonVariants, type ButtonProps }
+export { Button, buttonVariants, type ButtonProps };
--- frontend/src/share/ui/Buttons/Button/variants.ts
+++ frontend/src/share/ui/Buttons/Button/variants.ts
@@ -1,17 +1,31 @@
+import { cn } from "@share/packages/tailwindHelpers";
 import { type VariantProps, cva } from "class-variance-authority";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center gap-sm rounded-sm uppercase outline-none transition-all ring-offset-width-2 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-secondary-90 disabled:pointer-events-none",
+	cn(
+		"inline-flex items-center justify-center gap-sm rounded-sm uppercase outline-none transition-all ring-offset-width-2",
+		"focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-secondary-90",
+		"disabled:cursor-not-allowed",
+	),
 	{
 		variants: {
 			variant: {
-				primary: "bg-primary-60 text-base-180 hover:bg-primary-80",
-				secondary: "bg-base-160 text-base-0 hover:bg-base-150",
-				outline:
+				primary: cn(
+					"bg-primary-60 text-base-180 hover:bg-primary-80",
+					"disabled:text-base-120 disabled:bg-base-160",
+				),
+				secondary: cn(
+					"bg-base-160 text-base-0 hover:bg-base-150",
+					"disabled:text-base-120 disabled:bg-base-160",
+				),
+				outline: cn(
 					"text-primary-60 ring-[2px] ring-primary-60 hover:text-primary-80 hover:ring-primary-70 hover:ring-[4px]",
-				ghost: "text-primary-60 hover:text-primary-80",
-				disabled:
-					"text-base-60 bg-base-160 transition-none cursor-not-allowed focus-visible:ring-transparent",
+					"disabled:ring-base-160",
+				),
+				ghost: cn(
+					"text-primary-60 hover:text-primary-80",
+					"disabled:text-base-120",
+				),
 			},
 			size: {
 				lg: "px-sm py-[15x] text-contentLarge text-medium",
