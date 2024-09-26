960ceef8f640780b9c2e48b27cb59f51345ae5fc
Alian <iskra23231@gmail.com>
2024-09-26 23:33:30 +0300

    fix: city entity;

--- frontend/src/entity/city/api/getDetailCity.ts
+++ frontend/src/entity/city/api/getDetailCity.ts
@@ -1,13 +1,15 @@
-import { API_DOMAIN } from "@share/constants/API_DOMAIN"
-import { i18nConfig } from "@share/i18n"
-import { serverFetcher } from "@share/packages/fetcher"
-import { detailCitySchema } from "@share/schemas"
+import { API_DOMAIN } from "@share/constants/API_DOMAIN";
+import { serverFetcher } from "@share/packages/fetcher";
+import { detailCitySchema } from "@share/schemas";
+import { getLocale } from "next-intl/server";
 
-export async function getDetailCity(citySlug: string, lang = i18nConfig.defaultLocale) {
+export async function getDetailCity(citySlug: string, locale?: string) {
+	const lang = locale || (await getLocale());
+
 	return await serverFetcher({
 		name: "getDetailCity",
 		url: `${API_DOMAIN}/${lang}/api/v1/cities/city/${citySlug}`,
 		method: "GET",
 		responseSchema: detailCitySchema,
-	})
+	});
 }
--- frontend/src/entity/city/model/serverActions/getCityByIdAction.ts
+++ frontend/src/entity/city/model/serverActions/getCityByIdAction.ts
@@ -1,20 +1,21 @@
-"use server"
+"use server";
 
-import { z } from "zod"
-import { createServerAction } from "zsa"
-import { getAllCities } from "../../api/getAllCities"
-import { cityItemSchema } from "@share/schemas"
+import { isErrorResponse } from "@share/packages/fetcher";
+import { cityItemSchema } from "@share/schemas";
+import { z } from "zod";
+import { createServerAction } from "zsa";
+import { getAllCities } from "../../api/getAllCities";
 
 export const getCitiesByIdAction = createServerAction()
 	.input(cityItemSchema.pick({ id: true }))
 	.output(cityItemSchema.or(z.null()))
 	.handler(async ({ input }) => {
-		const cities = await getAllCities()
+		const cities = await getAllCities();
 
-		if ("code" in cities) {
-			return null
+		if (isErrorResponse(cities)) {
+			return null;
 		}
 
-		const filter = cities.filter((city) => city.id === input.id)
-		return filter[0] || null
-	})
+		const filter = cities.filter((city) => city.id === input.id);
+		return filter[0] || null;
+	});
